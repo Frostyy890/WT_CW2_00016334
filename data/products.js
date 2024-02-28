efe25446da5b86efca3c1f5dde16bb2687c5afa2
Abdurahmon <abdurahmon2004nuriy@gmail.com>
2024-02-29 03:42:37 +0500

    added a route and the page for a single item, changed stylings in the homepage

--- data/products.js
+++ data/products.js
@@ -1,8 +1,41 @@
-import { Product } from "../src/models/index.js"
+import { Product } from "../src/models/index.js";
 
 const Products = [
-    new Product(1234567890123, "Apple watch", 125.99, "Lorem ipsum dolor sit amet consectetur adipisicing elit", 5),
-    new Product(9731947981723, "Asus Rog Laptop", 1349.68, "Asus Rog series is now featuring new cutting edge laptop", 3)
-]
+  new Product(
+    1234567890123,
+    "Apple watch",
+    125.99,
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    5
+  ),
+  new Product(
+    9731947981723,
+    "Asus Rog Laptop",
+    1349.68,
+    "Asus Rog series is now featuring new cutting edge laptop",
+    3
+  ),
+  new Product(
+    9731947943723,
+    "Samsung Galaxy S24",
+    961.99,
+    "Brand new Samsung Galaxy S24",
+    3
+  ),
+  new Product(
+    9731947724238,
+    "Samsung Galaxy S23",
+    861.99,
+    "Brand new Samsung Galaxy S23",
+    3
+  ),
+  new Product(
+    9731947943723,
+    "iPhone 15",
+    962.99,
+    "Brand new Samsung Apple iPhone",
+    3
+  ),
+];
 
 export default Products;
--- src/routes/web/index.js
+++ src/routes/web/index.js
@@ -1,23 +1,32 @@
-import express from "express"
+import express from "express";
 import Users from "../../../data/users.js";
 import Products from "../../../data/products.js";
 
-
 const router = express.Router();
 router.get("/sign-up", (req, res) => {
-    res.render("sign-up")
+  res.render("sign-up");
 });
 router.get("/", (req, res) => {
-    res.render("home", {title: "Home Page", products: Products})
+  res.render("home", { title: "Home Page", products: Products });
 });
 
-router.get("/dashboard", (req, res)=> {
-    res.render("dashboard", {title: "Dashboard Page", users: Users})
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", { title: "Dashboard Page", users: Users });
 });
 
-router.get("/about", (req, res)=> {
-    res.render("about", {title: "About Page"})
-})
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About Page" });
+});
 
+router.get("/item/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const product = Products.find((product) => product.id === id);
+  console.log(product);
+  if (!product) {
+    return res.status(404).send("Product not found");
+  }
 
-export {router as webRouter}
+  res.render("item", { product: product });
+});
+
+export { router as webRouter };
